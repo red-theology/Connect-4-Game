57b4d277f5e927de5daf85b8ac5cd570e365d679
Sam Edwards <samuel.a.edwards5@gmail.com>
2020-11-02 12:05:00 -0500

    Second Commit

--- connect4.js
+++ connect4.js
@@ -5,8 +5,8 @@
  * board fills (tie)
  */
 
-let WIDTH = 7;
-let HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 let player = 1;
@@ -29,18 +29,22 @@ function makeBoard() {
 	}
 }
 const makeHtmlBoard = () => {
-	// TODO: add comment for this code
 	const numRows = board.length;
+	// store length/number of array in a variable. This gives us the number of rows we will work with
 	const numColumns = board[0].length;
-	const groupOfCols = document.createElement('colgroup');
-	makeTable.append(groupOfCols);
-	// create header cells to click on to drop checker
-	for (let c = 0; c < numColumns; c++) {
-		const headCol = document.createElement('col');
-		headCol.setAttribute('data-col', c);
-		groupOfCols.append(headCol);
-	}
+	// this will give us the number of columns we will work with. Could be any index number. We just need 7 columns.
+	// const groupOfCols = document.createElement('colgroup');
+	// // create group elements for the top of the game
+	// makeTable.append(groupOfCols);
+	// // create header cells to click on to drop checker
+	// for (let c = 0; c < numColumns; c++) {
+	// 	// loop through 7 columns, create column elements, give each unique data-attribute, append to the DOM
+	// 	const headCol = document.createElement('col');
+	// 	headCol.setAttribute('data-col', c);
+	// 	groupOfCols.append(headCol);
+	// }
 	for (let x = 0; x < numColumns; x++) {
+		// make TH elements to click on to drop checker into the board. Dynamically set attribute to be able to later correspond with tds.
 		const headCell = document.createElement('th');
 		headCell.className = 'header';
 		headCell.setAttribute('data-top', x);
@@ -48,6 +52,7 @@ const makeHtmlBoard = () => {
 		makeTable.append(headCell);
 	}
 	for (let tr = numRows; tr > 0; tr--) {
+		// create the elements to the table. Create 7 tds for every tr. This will give us our game board.
 		let makeTR = document.createElement('tr');
 		makeTR.setAttribute('data-row', tr);
 		makeTable.append(makeTR);
@@ -58,40 +63,64 @@ const makeHtmlBoard = () => {
 		}
 	}
 };
+// Create event listener to activate once the TH elements are clicked. Will run a loop to check to see if the value of the row is >= 0.
+//Row number decrements every time through the loop if the bottom cell has been filled until it reaches -1, at which point the loop terminates.
+//
 
-makeTable.addEventListener('click', (ev) => {
+function handleHeaderClick(ev) {
 	if (ev.target.className.includes('header')) {
 		const dataCol = ev.target.getAttribute('data-top');
 		const col = parseInt(dataCol, 10);
-		let row = board.length - 1;
-		while (row >= 0 && board[row][col]) {
-			row--;
-		}
-		if (row >= 0) {
-			// valid move
-			board[row][col] = player; // update app state
-			// update DOM
-			const cellIndex = row * WIDTH + col;
-			gameBoard.querySelectorAll('td')[cellIndex].style.backgroundColor = player === 1 ? 'yellow' : 'red';
-			console.log(`Player # ${player} Row: # ${row}`);
-			if (checkForWin()) {
-				endGame(`Player ${player} wins!`);
-			}
-			player = player === 1 ? 2 : 1;
-			// if(player === 1) {
-			//   player = 2;
-			// } else {
-			//   player = 1;
-			// }
-		}
+		// change value from string to a number
+		rowCheck(col);
 	}
-});
+}
+
+function clickBoard() {
+	makeTable.addEventListener('click', handleHeaderClick);
+}
+
+const rowCheck = (c) => {
+	let row = board.length - 1;
+	while (row >= 0 && board[row][c]) {
+		row--;
+	}
+	if (row >= 0) {
+		// valid move
+		board[row][c] = player; // update app state
+		// update DOM
+		const cellIndex = row * WIDTH + c;
+		// This allows us to fill the unique cell with the proper color
+		gameBoard.querySelectorAll('td')[cellIndex].style.backgroundColor = player === 1 ? 'yellow' : 'red';
+		// This fills the td with the appropriate color. If it is player 1, it will use yellow; if player 2, it will use red
+		// The conditional below will check to see if a player has won and the game is over. I still need to remove the event listner if the game is over.
+		gameOver();
+	}
+};
+const changePlayer = () => {
+	player = player === 1 ? 2 : 1;
+	// if(player === 1) {
+	//   player = 2;
+	// } else {
+	//   player = 1;
+	// }
+};
+
+const gameOver = () => {
+	if (checkForWin()) {
+		endGame(`Player ${player} wins!`);
+		makeTable.removeEventListener('click', handleHeaderClick);
+	}
+	// checkForTie();
+	changePlayer();
+};
+
 const endGame = (msg) => {
 	const newWinner = document.getElementById('winner');
 	newWinner.textContent = msg;
 	const resetButton = document.createElement('button');
 	resetButton.textContent = 'NEW GAME';
-	newWinner.append(resetButton);
+	newWinner.appendChild(resetButton);
 
 	resetButton.addEventListener('click', function(ev) {
 		window.location.reload();
@@ -99,25 +128,23 @@ const endGame = (msg) => {
 };
 
 const checkForWin = () => {
-	function _win(cells) {
+	function win(cells) {
 		// Check four cells to see if they're all color of current player
 		//  - cells: list of four (y, x) cells
-		//  - returns true if all are legal coordinates & all match currPlayer
-
-		// please help explain
+		//  - returns true if all are legal coordinates & all match player
+		// .every() method returns a win because it searches to see if the coordinates from the loop (that the function is run thru) fits the specs for getting 4 in a row.
 		return cells.every(([ y, x ]) => y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH && board[y][x] === player);
 	}
 
-	// TODO: read and understand this code. Add comments to help you. I have little clue how this works
-
+	// Loop checks each individual coordinate to see if it matches either horizontally, vertically, diagonally to the right, or diagonally to the left.
+	// Checks four coordinates for consecutive matches
 	for (let y = 0; y < HEIGHT; y++) {
 		for (let x = 0; x < WIDTH; x++) {
-			var horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
-			var vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
-			var diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
-			var diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
-
-			if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+			let horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
+			let vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
+			let diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
+			let diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
+			if (win(horiz) || win(vert) || win(diagDR) || win(diagDL)) {
 				return true;
 			}
 		}
@@ -126,3 +153,15 @@ const checkForWin = () => {
 
 makeBoard();
 makeHtmlBoard();
+clickBoard();
+
+// const checkForTie = () => {
+// 	for (let i = 0; i < board.length; i++) {
+// 		for (let n = 0; n < board.length + 1; n++) {
+// 			if (board[n][i].includes(0) !== true) {
+// 				alert('The game is a tie! Time for a new round!');
+// 				window.location.reload();
+// 			}
+// 		}
+// 	}
+// };
